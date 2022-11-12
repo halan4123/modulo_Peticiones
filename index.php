07617e5392fe28efcaf7c5b33273e7ae240ff740
Alan <halan4123@gmail.com>
2022-11-12 07:26:26 -0700

    Cambuiando el titulo de nuevo

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ include 'filtros.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Modulo de peticiones SLA</title>
+    <title>Modulo de peticiones</title>
 
 
     <!--CDN jQuery library -->
